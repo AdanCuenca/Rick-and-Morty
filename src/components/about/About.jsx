3d41ca804fe46dba27a60f358524f83181b767bd
Adan Cuenca <adancuenca@gmail.com>
2022-12-08 10:40:32 -0300

    rick and morty inicio-route

---
+++ src/components/about/About.jsx
@@ -0,0 +1,13 @@
+import styles from './About.module.css'
+import perfil from '../../assets/imagen-perfil.jpg'
+import React from 'react';
+
+export default function About(props) {
+  //   console.log(props);
+  return (
+    <div className={styles.about}>
+      <p>Mi informacion....</p>
+      
+    </div>
+  );
+}
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import SearchBar from "../searchbar/SearchBar";
+import {Link } from "react-router-dom";
+import styles from "./Nav.module.css"
+
+export default function Nav({onSearch}) {
+    
+    return (
+      <div className={styles.Nav}>
+        <Link to='/home'>Home</Link>
+        <br/>
+        <Link to='/about'>About</Link>
+        <SearchBar onSearch={onSearch}/>
+      </div>
+    );
+  }
+  
